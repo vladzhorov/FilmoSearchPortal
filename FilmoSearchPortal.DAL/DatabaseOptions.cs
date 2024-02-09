064e3c74ad81795bbc193235aea55f568784142e
vladzhorov <zhorovv7@gmail.com>
2024-02-09 03:47:11 +0300

    Update DatabaseOptions.cs

--- FilmoSearchPortal.DAL/DatabaseOptions.cs
+++ FilmoSearchPortal.DAL/DatabaseOptions.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FilmoSearchPortal.DAL
+﻿namespace FilmoSearchPortal.DAL
 {
-    internal class DatabaseOptions
+    public class DatabaseOptions
     {
+        public string ConnectionString { get; set; }
     }
 }
