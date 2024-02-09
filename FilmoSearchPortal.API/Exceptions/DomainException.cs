aa3a515173316b818d2fe16528756ede9fe25775
vladzhorov <zhorovv7@gmail.com>
2024-02-09 18:13:21 +0300

    Add DomainException

---
+++ FilmoSearchPortal.API/Exceptions/DomainException.cs
@@ -0,0 +1,21 @@
+﻿namespace FilmoSearchPortal.API.Exceptions
+{
+    [Serializable]
+    public abstract class DomainException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; }
+
+        protected DomainException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            HttpStatusCode = statusCode;
+        }
+
+        protected DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+
+        public abstract string Code { get; }
+    }
+}
