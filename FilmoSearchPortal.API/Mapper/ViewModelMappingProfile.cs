e0f5a5d53345fb3398967b677ed9c1853af2f496
vladzhorov <zhorovv7@gmail.com>
2024-02-09 03:54:07 +0300

    Add Mapping

---
+++ FilmoSearchPortal.API/Mapper/ViewModelMappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FilmoSearchPortal.API.ViewModels.Actor;
+using FilmoSearchPortal.API.ViewModels.Film;
+using FilmoSearchPortal.API.ViewModels.Review;
+using FilmoSearchPortal.API.ViewModels.User;
+using FilmoSearchPortal.BLL.Models;
+using FilmoSearchPortal.DAL.Entites;
+
+namespace FilmoSearchPortal.API.Mapping
+{
+    public class ViewModelMappingProfile : Profile
+    {
+        public ViewModelMappingProfile()
+        {
+
+            CreateMap<Film, FilmViewModel>();
+            CreateMap<Actor, ActorViewModel>();
+            CreateMap<Review, ReviewViewModel>();
+
+
+            CreateMap<FilmViewModel, Film>();
+            CreateMap<ActorViewModel, Actor>();
+            CreateMap<ReviewViewModel, Review>();
+
+
+            CreateMap<CreateFilmViewModel, Film>();
+            CreateMap<UpdateFilmViewModel, Film>();
+
+            CreateMap<CreateActorViewModel, Actor>();
+            CreateMap<UpdateActorViewModel, Actor>();
+
+            CreateMap<CreateReviewViewModel, Review>();
+            CreateMap<UpdateReviewViewModel, Review>();
+
+
+            CreateMap<UserViewModel, UserEntity>();
+            CreateMap<UserEntity, UserViewModel>();
+        }
+    }
+}
