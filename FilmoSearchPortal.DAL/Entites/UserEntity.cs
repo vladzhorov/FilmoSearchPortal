5402e91e39475f600277fdff0c5cf8e7c8b16586
vladzhorov <zhorovv7@gmail.com>
2024-02-12 17:03:45 +0300

    Add userid with id value

--- FilmoSearchPortal.DAL/Entites/UserEntity.cs
+++ FilmoSearchPortal.DAL/Entites/UserEntity.cs
@@ -4,5 +4,11 @@
     {
         public string Username { get; set; }
         public ICollection<ReviewEntity> Reviews { get; set; }
+
+        public Guid UserId { get; set; }
+        public UserEntity()
+        {
+            UserId = Id;
+        }
     }
 }
