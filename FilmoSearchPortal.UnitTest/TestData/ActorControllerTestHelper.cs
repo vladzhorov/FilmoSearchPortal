1ce0404210aeade85422ff8348b0d3c232cfd79e
vladzhorov <zhorovv7@gmail.com>
2024-02-12 16:57:29 +0300

    Add UnitTestHelpers

---
+++ FilmoSearchPortal.UnitTest/TestData/ActorControllerTestHelper.cs
@@ -0,0 +1,52 @@
+﻿using FilmoSearchPortal.API.ViewModels.Actor;
+using FilmoSearchPortal.BLL.Models;
+
+namespace FilmoSearchPortal.UnitTest.TestData
+{
+    public static class ActorControllerTestHelper
+    {
+        public static (List<Actor> actors, List<ActorViewModel> actorViewModels) TestGetAll()
+        {
+            var actorId1 = Guid.NewGuid();
+            var actorId2 = Guid.NewGuid();
+
+            var actors = new List<Actor>
+            {
+                new Actor { Id = actorId1, FirstName = "Actor 1" , LastName ="One"  },
+                new Actor { Id = actorId2, FirstName = "Actor 2" , LastName ="Two" }
+            };
+
+            var actorViewModels = new List<ActorViewModel>
+            {
+                new ActorViewModel { Id = actorId1,  FirstName = "Actor 1" , LastName ="One" },
+                new ActorViewModel { Id = actorId2, FirstName = "Actor 2" , LastName ="Two" }
+            };
+
+            return (actors, actorViewModels);
+        }
+
+        public static (Guid actorId, Actor actor, ActorViewModel actorViewModel) TestGetById()
+        {
+            var actorId = Guid.NewGuid();
+            var actor = new Actor { Id = actorId, FirstName = "TestActor", LastName = "TestActor" };
+            var actorViewModel = new ActorViewModel { Id = actorId, FirstName = "TestActor", LastName = "TestActor" };
+            return (actorId, actor, actorViewModel);
+        }
+
+        public static (CreateActorViewModel viewModel, Actor actor) TestCreate()
+        {
+            var actorId = Guid.NewGuid();
+            var viewModel = new CreateActorViewModel { FirstName = "TestActor", LastName = "TestActor" };
+            var actor = new Actor { Id = actorId, FirstName = viewModel.FirstName, LastName = viewModel.LastName };
+            return (viewModel, actor);
+        }
+
+        public static (Guid actorId, UpdateActorViewModel viewModel, Actor actor) TestUpdate()
+        {
+            var actorId = Guid.NewGuid();
+            var viewModel = new UpdateActorViewModel { FirstName = "Updated ", LastName = "Actor" };
+            var actor = new Actor { Id = actorId, FirstName = viewModel.FirstName, LastName = viewModel.LastName };
+            return (actorId, viewModel, actor);
+        }
+    }
+}
---
+++ FilmoSearchPortal.UnitTest/TestData/FilmControllerTestHelper.cs
@@ -0,0 +1,71 @@
+﻿using FilmoSearchPortal.API.ViewModels.Film;
+using FilmoSearchPortal.BLL.Models;
+
+namespace FilmoSearchPortal.UnitTest.TestData
+{
+    public static class FilmControllerTestHelper
+    {
+        public static (List<Film> films, List<FilmViewModel> filmViewModels) TestGetAll()
+        {
+            var filmId1 = Guid.NewGuid();
+            var filmId2 = Guid.NewGuid();
+
+            var films = new List<Film>
+            {
+                new Film { Id = filmId1, Title = "Film 1" },
+                new Film { Id = filmId2, Title = "Film 2" }
+            };
+
+            var filmViewModels = new List<FilmViewModel>
+            {
+                new FilmViewModel { Id = filmId1, Title = "Film 1" },
+                new FilmViewModel { Id = filmId2, Title = "Film 2" }
+            };
+
+            return (films, filmViewModels);
+        }
+
+        public static (Guid filmId, Film film, FilmViewModel filmViewModel) TestGetById()
+        {
+            var filmId = Guid.NewGuid();
+            var film = new Film { Id = filmId, Title = "Test Film" };
+            var filmViewModel = new FilmViewModel { Id = filmId, Title = "Test Film" };
+            return (filmId, film, filmViewModel);
+        }
+
+        public static (CreateFilmViewModel viewModel, Film film) TestCreate()
+        {
+            var filmId = Guid.NewGuid();
+            var viewModel = new CreateFilmViewModel { Title = "Test Film" };
+            var film = new Film { Id = filmId, Title = viewModel.Title };
+            return (viewModel, film);
+        }
+
+        public static (Guid filmId, UpdateFilmViewModel viewModel, Film film) TestUpdate()
+        {
+            var filmId = Guid.NewGuid();
+            var viewModel = new UpdateFilmViewModel { Title = "Updated Film" };
+            var film = new Film { Id = filmId, Title = viewModel.Title };
+            return (filmId, viewModel, film);
+        }
+
+        public static Guid TestDelete()
+        {
+            return Guid.NewGuid();
+        }
+
+        public static (Guid filmId, ICollection<Guid> actorIds, Film film, IEnumerable<Actor> actors, FilmViewModel updatedFilmViewModel) TestAddActorsToFilm()
+        {
+            var filmId = Guid.NewGuid();
+            var actorIds = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+            var film = new Film { Id = filmId, Title = "Film 1" };
+            var actors = new List<Actor>
+            {
+                new Actor { Id = actorIds[0], FirstName = "Actor 1",LastName = "One" },
+                new Actor { Id = actorIds[1], FirstName = "Actor 2", LastName = "Two" }
+            };
+            var updatedFilmViewModel = new FilmViewModel { Id = filmId, Title = "Film" };
+            return (filmId, actorIds, film, actors, updatedFilmViewModel);
+        }
+    }
+}
