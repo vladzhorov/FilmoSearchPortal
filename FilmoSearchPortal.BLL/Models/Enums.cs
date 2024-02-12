44e6b5548c5b421bfd966ef5b193d3dd0bec4d2c
vladzhorov <zhorovv7@gmail.com>
2024-02-12 17:00:22 +0300

    Add Enums(Bll)

---
+++ FilmoSearchPortal.BLL/Models/Enums.cs
@@ -0,0 +1,34 @@
+﻿namespace FilmoSearchPortal.BLL.Models
+{
+    public class Enums
+    {
+        public enum ActorStatus
+        {
+            Active,
+            Retired
+        }
+        public enum Genre
+        {
+            Animation,
+            Western,
+            Eastern,
+            Thriller,
+            Suspense,
+            Adventure,
+            Horrors,
+            Comedy,
+            Parody,
+            Drama,
+            Melodrama,
+            Tragicomedia,
+            Mystique,
+            Fantasy,
+            Cyberpunk,
+            Biography,
+            Documentary,
+            Erotica,
+            Sport
+
+        }
+    }
+}
