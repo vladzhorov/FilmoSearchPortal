ba7578aaf8b94b624c4b49efbe60ad6ebcb81e70
vladzhorov <zhorovv7@gmail.com>
2024-02-12 17:13:54 +0300

    Add feature for GetAll(FilmService)

--- FilmoSearchPortal.BLL/Services/FilmService.cs
+++ FilmoSearchPortal.BLL/Services/FilmService.cs
@@ -1,24 +1,49 @@
 ﻿using AutoMapper;
 using FilmoSearchPortal.BLL.Abstractions.Services;
 using FilmoSearchPortal.BLL.Models;
+using FilmoSearchPortal.DAL;
 using FilmoSearchPortal.DAL.Entites;
 using FilmoSearchPortal.DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilmoSearchPortal.BLL.Services
 {
     public class FilmService : GenericService<FilmEntity, Film>, IFilmService
     {
-        private readonly IFilmRepository _filmRepository;
+        private readonly AppDbContext _context;
 
-        public FilmService(IFilmRepository filmRepository, IMapper mapper)
-            : base(filmRepository, mapper)
+        public FilmService(IRepository<FilmEntity> repository, IMapper mapper, AppDbContext context) : base(repository, mapper)
         {
-            _filmRepository = filmRepository;
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Actor>> GetActorsForFilmAsync(Guid filmId, CancellationToken cancellationToken)
+        {
+            var film = await _context.Films.Include(f => f.Actors).FirstOrDefaultAsync(f => f.Id == filmId, cancellationToken);
+            return _mapper.Map<IEnumerable<Actor>>(film.Actors);
+        }
+
+        public async override Task<IEnumerable<Film>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var filmsWithActorsAndReviews = await _context.Films
+                .Include(f => f.Actors)
+                .Include(f => f.Reviews)
+                .ToListAsync(cancellationToken);
+
+            var models = _mapper.Map<IEnumerable<Film>>(filmsWithActorsAndReviews);
+            return models;
+        }
+
+        public async Task<IEnumerable<Review>> GetReviewsForFilmAsync(Guid filmId, CancellationToken cancellationToken)
+        {
+            var film = await _context.Films
+                .Include(f => f.Reviews)
+                .FirstOrDefaultAsync(f => f.Id == filmId, cancellationToken);
+
+            if (film == null || film.Reviews == null)
+                return Enumerable.Empty<Review>();
+
+            return _mapper.Map<IEnumerable<Review>>(film.Reviews);
         }
     }
 }
