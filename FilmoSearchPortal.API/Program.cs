d840a7aa84221e6faecc5c9768b2b8e86a259ded
vladzhorov <zhorovv7@gmail.com>
2024-02-14 04:26:26 +0300

    Fix Program

--- FilmoSearchPortal.API/Program.cs
+++ FilmoSearchPortal.API/Program.cs
@@ -1,91 +1,74 @@
-using FilmoSearchPortal.API.Controllers;
 using FilmoSearchPortal.API.Mapping;
 using FilmoSearchPortal.API.Middleware;
 using FilmoSearchPortal.API.Validators.Actor;
 using FilmoSearchPortal.API.Validators.Film;
 using FilmoSearchPortal.API.Validators.Review;
 using FilmoSearchPortal.API.Validators.User;
-using FilmoSearchPortal.BLL.Abstractions.Services;
-using FilmoSearchPortal.BLL.Services;
+using FilmoSearchPortal.BLL;
 using FilmoSearchPortal.DAL;
-using FilmoSearchPortal.DAL.Entites;
-using FilmoSearchPortal.DAL.Interfaces;
-using FilmoSearchPortal.DAL.Repostories;
 using FluentValidation.AspNetCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers()
-    .AddFluentValidation(fv =>
+public class Program
+{
+    public static void Main(string[] args)
     {
-        fv.RegisterValidatorsFromAssemblyContaining<CreateUserViewModelValidator>();
-        fv.RegisterValidatorsFromAssemblyContaining<CreateReviewViewModelValidator>();
-        fv.RegisterValidatorsFromAssemblyContaining<CreateFilmViewModelValidator>();
-        fv.RegisterValidatorsFromAssemblyContaining<CreateActorViewModelValidator>();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers()
+            .AddFluentValidation(fv =>
+            {
+                fv.RegisterValidatorsFromAssemblyContaining<CreateUserViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<CreateReviewViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<CreateFilmViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<CreateActorViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<UpdateUserViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<UpdateReviewViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<UpdateFilmViewModelValidator>();
+                fv.RegisterValidatorsFromAssemblyContaining<UpdateActorViewModelValidator>();
 
-    })
-     .AddJsonOptions(op =>
-{
-    op.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            })
+             .AddJsonOptions(op =>
+        {
+            op.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
 
-});
+        builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = " API ", Version = "v1" });
+        });
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = " API ", Version = "v1" });
-});
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
 
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
+        builder.Services.AddDALDependencies(configuration);
 
+        builder.Services.AddBLLDependencies();
 
+        builder.Services.AddAutoMapper(typeof(Program), typeof(ViewModelMappingProfile));
+        var app = builder.Build();
 
-builder.Services.AddDALDependencies(configuration, useInMemoryDatabase: true);
+        app.UseMiddleware<ExceptionMiddleware>();
 
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IActorRepository, ActorRepository>();
-builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-builder.Services.AddScoped<IFilmService, FilmService>();
-builder.Services.AddScoped<IActorService, ActorService>();
-builder.Services.AddScoped<IReviewService, ReviewService>();
-builder.Services.AddScoped<IFilmRepository, FilmRepository>();
-builder.Services.AddScoped<IRepository<FilmEntity>, FilmRepository>();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", " API ");
+            });
+        }
+        app.UseHttpsRedirection();
 
+        app.UseAuthorization();
 
-builder.Services.AddScoped<ReviewController>();
-builder.Services.AddScoped<FilmController>();
-builder.Services.AddScoped<ActorController>();
+        app.MapControllers();
 
-builder.Services.AddAutoMapper(typeof(Program), typeof(ViewModelMappingProfile));
-var app = builder.Build();
-
-
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", " API ");
-    });
+        app.Run();
+        //VULTURES
+    }
 }
-
-
-
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
