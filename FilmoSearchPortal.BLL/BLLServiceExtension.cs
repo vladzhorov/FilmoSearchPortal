db9ac589c779353b8fe0a695ba9a7a96091b0d66
vladzhorov <zhorovv7@gmail.com>
2024-02-14 04:27:23 +0300

    Create BLLServiceExtension.cs

---
+++ FilmoSearchPortal.BLL/BLLServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿using FilmoSearchPortal.BLL.Abstractions.Services;
+using FilmoSearchPortal.BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace FilmoSearchPortal.BLL
+{
+    public static class BLLServiceExtension
+    {
+        public static void AddBLLDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IFilmService, FilmService>();
+            services.AddScoped<IReviewService, ReviewService>();
+            services.AddScoped<IActorService, ActorService>();
+
+        }
+    }
+}
+
